63a6967760836fc0bfdfdb9f376e0749fdfca21f
brugdev <149311124+brugdev@users.noreply.github.com>
2024-01-13 18:52:40 -0300

    comando_custom_comprar

--- cypress/e2e/exercicio-e2e.cy.js
+++ cypress/e2e/exercicio-e2e.cy.js
@@ -14,60 +14,14 @@ context('Exercicio - Testes End-to-end - Fluxo de pedido', () => {
         cy.visit('/')
     });
 
-    it('Deve fazer um pedido na loja Ebac Shop de ponta a ponta', () => {//escolhendo os produtos
-        cy.get('.product-block ')//escolhendo primeiro produto
-        .contains('Ingrid Running Jacket').click()
-        cy.get('.button-variable-item-S').click()
-        cy.get('.button-variable-item-Red').click()
-        cy.get('.single_add_to_cart_button').click()//escolhendo primeiro produto
-        cy.get('.logo-in-theme > .logo > a > .logo-img').click()// retorno a pagina inicial
-        cy.get('.product-block ')//escolhendo segundo produto
-        .contains('Augusta Pullover Jacket').click()
-        cy.get('.button-variable-item-S').click()
-        cy.get('.button-variable-item-Red').click()
-        cy.get('.single_add_to_cart_button').click()//escolhendo segundo produto
-        cy.get('.logo-in-theme > .logo > a > .logo-img').click()
-        cy.get('.product-block ')//escolhendo terceiro produto
-        .contains('Josie Yoga Jacket').click()
-        cy.get('.button-variable-item-S').click()
-        cy.get('.button-variable-item-Blue').click()
-        cy.get('.single_add_to_cart_button').click()//escolhendo terceiro produto
-        cy.get('.logo-in-theme > .logo > a > .logo-img').click()// retorno a pagina inicial
-        cy.get('.product-block ')//escolhendo quarto produto
-        .contains('Eos V-Neck Hoodie').click()
-        cy.get('.button-variable-item-S').click()
-        cy.get('.button-variable-item-Blue').click()
-        cy.get('.single_add_to_cart_button').click()//escolhendo quarto produto
+
+    it('Deve comprar 4  um produto', () => {
+        cy.comprarProduto("Ingrid Running Jacket","Augusta Pullover Jacket","Josie Yoga Jacket","Stellar Solar Jacket" )
+
         
-        //finalizando a compra//
-        cy.get('#cart > .dropdown-toggle').click()
-        cy.get('#cart > .dropdown-menu > .widget_shopping_cart_content > .mini_cart_content > .mini_cart_inner > .mcart-border > .buttons > .checkout').click()
-        //finalizando a compra//
-
-        // ja cadastrado //
-        cy.get('.showlogin').click()
-        cy.get('#username').click().type('bruno.teste@teste.com.br')
-        cy.get('#password').click().type('teste@123')
-        cy.get('.woocommerce-button').click()
-        cy.get('#terms').click()
-        cy.get('#place_order').click()
-        cy.get('.woocommerce-notice').should('contain' , 'Obrigado. Seu pedido foi recebido.')
-
-
-
-
-
-
-        })
-
+    });
     
-        
-
-
-
-        
-
-         
+  
     });
 
 
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -31,3 +31,53 @@ Cypress.Commands.add('login', (usuario, senha) => {
     cy.get('.woocommerce-form > .button').click()
 });
 
+
+
+
+
+Cypress.Commands.add('comprarProduto', (produto1, produto2, produto3, produto4) => {
+    cy.get('.product-block ')//escolhendo primeiro produto
+    .contains(produto1).click()
+    cy.get('.button-variable-item-S').click()
+    cy.get('.button-variable-item-Red').click()
+    cy.get('.single_add_to_cart_button').click()//escolhendo primeiro produto
+    cy.get('.logo-in-theme > .logo > a > .logo-img').click()// retorno a pagina inicial
+    cy.get('.product-block ')//escolhendo segundo produto
+    .contains(produto2).click()
+    cy.get('.button-variable-item-S').click()
+    cy.get('.button-variable-item-Red').click()
+    cy.get('.single_add_to_cart_button').click()//escolhendo segundo produto
+    cy.get('.logo-in-theme > .logo > a > .logo-img').click()
+    cy.get('.product-block ')//escolhendo terceiro produto
+    .contains(produto3).click()
+    cy.get('.button-variable-item-S').click()
+    cy.get('.button-variable-item-Blue').click()
+    cy.get('.single_add_to_cart_button').click()//escolhendo terceiro produto
+    cy.get('.logo-in-theme > .logo > a > .logo-img').click()// retorno a pagina inicial
+    cy.get('.product-block ')//escolhendo quarto produto
+    .contains(produto4).click()
+    cy.get('.button-variable-item-S').click()
+    cy.get('.button-variable-item-Blue').click()
+    cy.get('.single_add_to_cart_button').click()//escolhendo quarto produto
+    
+    //finalizando a compra//
+    cy.get('#cart > .dropdown-toggle').click()
+    cy.get('#cart > .dropdown-menu > .widget_shopping_cart_content > .mini_cart_content > .mini_cart_inner > .mcart-border > .buttons > .checkout').click()
+    //finalizando a compra//
+
+    // ja  cadastrado //
+    cy.get('.showlogin').click()
+    cy.get('#username').click().type('bruno.teste@teste.com.br')
+    cy.get('#password').click().type('teste@123')
+    cy.get('.woocommerce-button').click()
+    cy.get('#terms').click()
+    cy.get('#place_order').click()
+    cy.get('.woocommerce-notice').should('contain' , 'Obrigado. Seu pedido foi recebido.')
+
+
+
+
+
+
+    })
+
